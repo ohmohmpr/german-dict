a6b84127f5aee40f75648ff7977638d6fd5bf9bb
Panyawat Rattana <panyr603@chare.postbox.in.th>
2022-04-23 13:12:50 +0700

    delete jade & add ejs

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const wordRouter = require('./routes/word');
 
 const app = express()
 const port = 3000
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/word', wordRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
