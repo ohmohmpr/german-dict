652700b739e565ef78317afc05ec050e6525190b
Panyawat Rattana <nanoic_ohm@hotmail.com>
2025-04-16 17:06:24 +0200

    add woerter db

--- create_table_saetze.sql
+++ create_table_saetze.sql
@@ -19,20 +19,6 @@ CREATE TABLE sätze (
     update_time timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE SEQUENCE verben_id_seq START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
-CREATE TABLE verben (
-    id bigint DEFAULT nextval('verben_id_seq'::regclass) NOT NULL PRIMARY KEY,
-    verb character varying(255) NOT NULL UNIQUE,
-    bedeutung character varying(255) NOT NULL,
-    create_time timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    update_time timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-INSERT INTO verben (verb, bedeutung) VALUES ('geben', 'give'), 
-('aufgeben', 'give up'), 
-('ausgeben', 'spend'), 
-('übergeben', 'throw up');
-
 CREATE SEQUENCE präposition_id_seq START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
 CREATE TABLE präpositionen (
     id bigint DEFAULT nextval('präposition_id_seq'::regclass) NOT NULL PRIMARY KEY,
@@ -65,4 +51,97 @@ SELECT * FROM sätze ORDER BY RANDOM() LIMIT 1;
 
 psql -U postgres
 
-select * from sätze where false;
+CREATE SEQUENCE verben_id_seq START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+CREATE TABLE verben (
+    id bigint DEFAULT nextval('verben_id_seq'::regclass) NOT NULL PRIMARY KEY,
+    verb character varying(255) NOT NULL UNIQUE,
+    bedeutung character varying(255) NOT NULL,
+    create_time timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    update_time timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+INSERT INTO verben (verb, bedeutung) VALUES ('geben', 'give'), 
+('aufgeben', 'give up'), 
+('ausgeben', 'spend'), 
+('übergeben', 'throw up');
+
+
+CREATE SEQUENCE wörter_id_seq START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+CREATE TABLE wörter (
+    id bigint DEFAULT nextval('wörter_id_seq'::regclass) NOT NULL PRIMARY KEY,
+    wort character varying(255) NOT NULL UNIQUE,
+    bedeutung character varying(255),
+    create_time timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    update_time timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+INSERT INTO wort (verb, bedeutung) VALUES ('geben', 'give'), 
+('aufgeben', 'give up'), 
+('ausgeben', 'spend'), 
+('übergeben', 'throw up');
+
+SELECT * FROM sätze where ASCII(left(satz, 1)) between ASCII('A') and ASCII('Z') ORDER BY update_time DESC;
+SELECT * FROM sätze where ASCII(left(satz, 1)) = ASCII('Ä') ORDER BY update_time DESC;
+SELECT * FROM sätze where ASCII(left(satz, 1)) = ASCII('Ö') ORDER BY update_time DESC;
+SELECT * FROM sätze where ASCII(left(satz, 1)) = ASCII('Ü') ORDER BY update_time DESC;
+SELECT * FROM sätze where ASCII(left(satz, 1)) = ASCII('ẞ') ORDER BY update_time DESC;
+
+SELECT satz FROM sätze;
+SELECT substring(satz from '^[A-Z]*') ;
+SELECT regexp_match();
+SELECT satz, (SELECT regexp_matches(satz, '^[A-Z].*')) FROM sätze;
+SELECT satz, (SELECT regexp_matches(satz, E'^[A-Z].*\\s+')) FROM sätze;
+-- SELECT 
+--     (
+       
+--             SELECT regexp_split_to_array( SELECT regexp_matches(satz, '[A-Z].*'), E'\\s+'), 
+--     ) as words 
+-- FROM sätze;
+-- SELECT * FROM regexp_split_to_array(satz, E'\\s+');
+
+-- SELECT words[:3]
+-- FROM (
+--         SELECT 
+--             (SELECT regexp_split_to_array(satz, E'\\s+')) as words
+--         FROM sätze
+--     ) arr;
+
+-- regexp_substr
+SELECT 
+    regexp_replace(
+        regexp_matches( 
+            UNNEST( regexp_split_to_array(satz, E'\\s+') ), '^[A-Z, Ä, Ö, Ü, ẞ].*'
+        ),
+        '.',
+        ''
+    )
+FROM sätze;
+
+SELECT 
+    DISTINCT regexp_replace(
+        UNNEST (regexp_matches( 
+            UNNEST( regexp_split_to_array(satz, E'\\s+') ), '^[A-Z, Ä, Ö, Ü, ẞ].*'
+            )
+        ),
+        '\.|\:|\,|!|\?',
+        ''
+    )
+FROM sätze;
+
+INSERT INTO wörter (wort)
+    (
+        SELECT 
+            DISTINCT regexp_replace(
+                UNNEST (regexp_matches( 
+                    UNNEST( regexp_split_to_array(satz, E'\\s+') ), '^[A-Z, Ä, Ö, Ü, ẞ].*'
+                    )
+                ),
+                '\.|\:|\,|!|\?',
+                ''
+            )
+        FROM sätze
+    );
+
+-- https://www.postgresql.org/docs/9.1/functions-string.html
+-- https://stackoverflow.com/questions/15625629/regex-expressions-in-java-s-vs-s
+-- https://www.postgresql.org/docs/current/functions-matching.html
